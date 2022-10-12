e32a2ca4de611fa2cafea6043f8740e3ac9d061e
Lenin <cmoposisan1@educacion.navarra.es>
2022-10-12 16:43:35 +0200

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pregunte el nombre del usuario
+# en la consola y un número entero e imprima por pantalla
+# en líneas distintas el nombre del usuario tantas veces
+# como el número introducido.
+
+nombre = str(input("Introduce tu nombre: "))
+numero = int(input("Introduce un numero: "))
+
+print((nombre + "\n") * numero)
